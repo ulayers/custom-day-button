19c759f85e1d9c04314dd57da99f8886f9270af6
mohali1988 <moh.ali19888@gmail.com>
2024-07-01 03:44:04 +0300

    rebuild uninstall file

--- uninstall.php
+++ uninstall.php
@@ -9,8 +9,3 @@
 if (!defined('WP_UNINSTALL_PLUGIN')) {
     exit;
 }
-
-global $wpdb;
-$wpdb->delete($wpdb->options, array('option_name' => 'wpil_settings'));
-$wpdb->delete($wpdb->options, array('option_name' => 'WPInstalogin_lic_Key'));
-$wpdb->query("DELETE FROM $wpdb->options WHERE option_name LIKE 'WPInstalogin_lic_Key_%'");
